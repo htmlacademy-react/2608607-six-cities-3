ff67da17164997e306487be4f368dd54ea285ce0
Yuri-tm <yuritm.tm@gmail.com>
2025-12-09 01:04:13 +0300

    добавляет компоненты tabs, cities, header

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+
+export default function Tabs(): JSX.Element {
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Paris</span>
+            </Link>
+          </li>
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Cologne</span>
+            </Link>
+          </li>
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Brussels</span>
+            </Link>
+          </li>
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Amsterdam</span>
+            </Link>
+          </li>
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Hamburg</span>
+            </Link>
+          </li>
+          <li className="locations__item">
+            <Link className="locations__item-link tabs__item" to="#">
+              <span>Dusseldorf</span>
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,109 +1,20 @@
-import { Link } from 'react-router-dom';
-import ProductCardList from '../../components/product-card-list/product-card-list';
+//import { Link } from 'react-router-dom';
+import Header from '../../components/header/header';
+import Tabs from '../../components/tabs/tabs';
+import Cities from '../../components/cities/cities';
 
-type MainPageProps = {
-  locationCount: number;
-}
+//type MainPageProps = {
+//locationCount: number;
+//}
 
-export default function MainPage({locationCount}: MainPageProps): JSX.Element {
+export default function MainPage(): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to="#todo">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <Link className="header__nav-link" to="#">
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Paris</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Cologne</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Brussels</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Amsterdam</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Hamburg</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Dusseldorf</span>
-                </Link>
-              </li>
-            </ul>
-          </section>
-        </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{locationCount} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                <ProductCardList locationCount={locationCount} /> {/*Renders a list of ProductCard components based on locationCount prop*/}
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
-          </div>
-        </div>
+        <Tabs />
+        <Cities />
       </main>
     </div>
   );
